2475ef15c2f5c2f94622b7b750123cbe90f84a12
liusilan <liusilan@douyu.tv>
2018-03-29 15:34:05 +0800

    Initial Commit

---
+++ PodfileEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PodfileEditor
+//
+//  Created by liusilan on 2018/3/29.
+//  Copyright © 2018年 summer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
