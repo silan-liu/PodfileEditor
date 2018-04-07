344f84f5cd053729e52702f1b47ca4b40e96dd27
silan-liu <413769707@qq.com>
2018-04-07 14:36:21 +0800

    添加command

--- PodfileEditor/Source/ProjectDetail/ProjectDetailViewController.swift
+++ PodfileEditor/Source/ProjectDetail/ProjectDetailViewController.swift
@@ -10,6 +10,14 @@ import Cocoa
 
 class ProjectDetailViewController: NSViewController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet var textView: NSTextView!
+    
+    var installCommand: CommandExecutor = {
+        let execuator = CommandExecutor(with: ["install", "--verbose"])
+        return execuator
+    }()
+    
     var projectInfo: ProjectInfo? {
         didSet {
             parsePodfile()
@@ -19,6 +27,44 @@ class ProjectDetailViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        textView.string = """
+        fkkkk2018-04-07 13:56:37.976367+0800 PodfileEditor[3690:110150] double click row, 1 \
+        2018-04-07 13:56:38.005014+0800 PodfileEditor[3690:110150] *** Illegal NSTableView data source (<PodfileEditor.ProjectDetailViewController: 0x608000142890>).Must implement numberOfRowsInTableView: and tableView:objectValueForTableColumn:row:\
+        projectInfo:~/Documents/test22018-04-07 13:56:37.976367+0800 PodfileEditor[3690:110150] double click row, 1\
+        2018-04-07 13:56:38.005014+0800 PodfileEditor[3690:110150] *** Illegal NSTableView data source (<PodfileEditor.ProjectDetailViewController: 0x608000142890>).  Must implement numberOfRowsInTableView: and tableView:objectValueForTableColumn:row:
+        projectInfo:~/Documents/test22018-04-07 13:56:37.976367+0800 PodfileEditor[3690:110150] double click row, 1
+        2018-04-07 13:56:38.005014+0800 PodfileEditor[3690:110150] *** Illegal NSTableView data source (<PodfileEditor.ProjectDetailViewController: 0x608000142890>).  Must implement numberOfRowsInTableView: and tableView:objectValueForTableColumn:row:
+        projectInfo:~/Documents/test22018-04-07 13:56:37.976367+0800 PodfileEditor[3690:110150] double click row, 1
+        2018-04-07 13:56:38.005014+0800 PodfileEditor[3690:110150] *** Illegal NSTableView data source (<PodfileEditor.ProjectDetailViewController: 0x608000142890>).  Must implement numberOfRowsInTableView: and tableView:objectValueForTableColumn:row:
+        projectInfo:~/Documents/test22018-04-07 13:56:37.976367+0800 PodfileEditor[3690:110150] double click row, 1
+        2018-04-07 13:56:38.005014+0800 PodfileEditor[3690:110150] *** Illegal NSTableView data source (<PodfileEditor.ProjectDetailViewController: 0x608000142890>).  Must implement numberOfRowsInTableView: and tableView:objectValueForTableColumn:row:
+        projectInfo:~/Documents/test2
+        """
+    }
+    
+    @IBAction func podInstallAction(_ sender: Any) {
+        self.updateCommandOutput(output: """
+\n
+========================================
+\n
+""")
+        installCommand.run { (output) in
+            self.updateCommandOutput(output: output)
+        }
+        
+        self.updateCommandOutput(output: """
+\n
+========================================
+\n
+""")
+    }
+    
+    func updateCommandOutput(output: String) {
+        DispatchQueue.main.async {
+            self.textView.string = self.textView.string.appending(output)
+            self.textView.scrollToEndOfDocument(nil)
+        }
     }
     
     func parsePodfile() {
---
+++ PodfileEditor/Source/Utils/CommandExecutor.swift
@@ -0,0 +1,54 @@
+//
+//  CommandExecutor.swift
+//  PodfileEditor
+//
+//  Created by silan on 2018/4/7.
+//  Copyright © 2018年 summer. All rights reserved.
+//
+
+import Cocoa
+
+class CommandExecutor: NSObject {
+    var arguments: [String]?
+    
+    init(with arguments: [String]) {
+        super.init()
+        self.arguments = arguments
+    }
+    
+    func run(withHandler outputHandler: ((String) -> ())? = nil) {
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        let process = Process()
+        
+        process.launchPath = "/usr/local/bin/pod"
+        process.arguments = arguments
+        process.standardOutput = outputPipe
+        process.standardError = errorPipe
+        
+        if outputHandler != nil {
+            
+            outputPipe.fileHandleForReading.readabilityHandler = { pipe in
+                if let line = String(data: pipe.availableData, encoding: .utf8) {
+                    outputHandler?(line)
+                }
+            }
+            
+            errorPipe.fileHandleForReading.readabilityHandler = { pipe in
+                if let line = String(data: pipe.availableData, encoding: .utf8) {
+                    outputHandler?(line)
+                }
+            }
+            
+            process.terminationHandler = { _ in
+                outputPipe.fileHandleForReading.readabilityHandler = nil
+                errorPipe.fileHandleForReading.readabilityHandler = nil
+            }
+        }
+        
+        process.launch()
+        process.waitUntilExit()
+    }
+}
+
+
