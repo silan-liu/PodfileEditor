78e05aeba36b382203ee77cbef680bb8db0a139b
silan-liu <413769707@qq.com>
2018-04-16 20:41:00 +0800

    添加ProjectDetailDataController

---
+++ PodfileEditor/Source/ProjectDetail/DataController/ProjectDetailDataController.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectDetailDataController.swift
+//  PodfileEditor
+//
+//  Created by liusilan on 2018/4/16.
+//  Copyright © 2018年 summer. All rights reserved.
+//
+
+import Cocoa
+
+class ProjectDetailDataController: NSObject {
+
+}
